fa5cb3306d67266846c60093eb3edb22f0a7fb90
Ugo Schieli <ugo.schieli@gmail.com>
2023-07-27 22:35:09 +0200

    Automatic building of the iso

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xe
+
+cp -r /usr/share/archiso/configs/releng build/releng
+cp -r ./src ./build/releng/airootfs/root
+sudo mkarchiso -v -w /tmp/archiso-tmp -o ./build/out ./build/releng
+sudo rm -rf /tmp/archiso-tmp
