5fb40b1b6eb6ff26d9afe4e05056b07ec7c39dee
ugoat <ugo.schieli@gmail.com>
2023-07-27 22:36:20 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -xe
 
 cp -r /usr/share/archiso/configs/releng build/releng
-cp -r ./src ./build/releng/airootfs/root
+cp -r ./src ./build/releng/airootfs/root/archinstall
 sudo mkarchiso -v -w /tmp/archiso-tmp -o ./build/out ./build/releng
 sudo rm -rf /tmp/archiso-tmp
