bfef379eebc90574dbac98fb85779f8d0bba7191
Ugo Schieli <ugo.schieli@gmail.com>
2023-07-27 12:46:21 +0200

    Here we go

---
+++ chroot.sh
@@ -0,0 +1,43 @@
+echo "Hello from chroot"
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+sed '/en_US.UTF-8 UTF-8/s/^#//' -i /etc/locale.gen
+locale-gen
+
+cat << EOF > /etc/locale.conf
+LANG=en_US.UTF-8
+EOF
+
+cat << EOF > /etc/vconsole.conf
+KEYMAP=fr
+EOF
+
+cat << EOF > /etc/hostname
+gemini
+EOF
+
+systemctl enable NetworkManager.service
+
+passwd
+
+useradd -m -G adm,ftp,games,http,rfkill,sys,systemd-journal,log,uucp,wheel ugo
+sed '/%wheel ALL=(ALL:ALL) ALL/s/^# //' -i /etc/sudoers
+passwd ugo
+
+sed '/Color/s/^#//' -i /etc/pacman.conf
+sed '/ParallelDownloads/s/^#//' -i /etc/pacman.conf
+
+bootctl install
+cat << EOF > /boot/loader/loader.conf
+default arch.conf
+EOF
+cat << EOF > /boot/loader/entries/arch.conf
+title     Arch Linux
+linux     /vmlinuz-linux
+initrd    /amd-ucode.img
+initrd    /initramfs-linux.img
+options   root=PARTLABEL=ROOT_PART rw quiet
+EOF
+systemctl enable systemd-boot-update.service
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+DISK=/dev/nvme0n1
+
+if [[ "$DISK" =~ "nvme" ]]; then
+    ESP="${DISK}p1";
+    SWAP_PART="${DISK}p2";
+    ROOT_PART="${DISK}p3";
+elif [[ "$DISK" =~ "sd" ]]; then
+    EFI_PART="${DISK}1";
+    SWAP_PART="${DISK}2";
+    ROOT_PART="${DISK}3";
+fi
+
+parted --script $DISK mklabel gpt \
+             mkpart '"ESP"' fat32 1MiB 501MiB \
+             set 1 esp on \
+             mkpart '"SWAP_PART"' linux-swap 501MiB 10.5GiB \
+             mkpart '"ROOT_PART"' ext4 10.5Gib 100%
+
+mkfs.fat -F 32 -n "EFI" $ESP
+mkswap -L "SWAP" $SWAP_PART
+mkfs.ext4 -L "ROOT_FS" $ROOT_PART
+
+mount $ROOT_PART /mnt
+mount --mkdir $ESP /mnt/boot
+swapon $SWAP_PART
+
+reflector --country fr,de --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap -K /mnt - < pkglist.txt
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp chroot.sh /mnt
+arch-chroot /mnt ./chroot.sh
+
+echo 'Installation finished'
