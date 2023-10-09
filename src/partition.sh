eeaa5f55315f129c77c99c9df8e6af17a621aa26
ugoat <ugo.schieli@gmail.com>
2023-10-09 20:54:15 +0200

    Update partition.sh

--- src/partition.sh
+++ src/partition.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "name=esp,type=uefi,bootable,size=+500MiB" | sfdisk /dev/sda --quiet
-echo "name=swap,type=swap,size=+1GiB" | sfdisk /dev/sda --append --quiet
-echo "name=root,type=linux,size=+10GiB" | sfdisk /dev/sda --append --quiet
-echo "name=home,type=linux" | sfdisk /dev/sda --append --quiet
+echo "name=esp,type=uefi,bootable,size=+500MiB" | sfdisk /dev/sda --quiet --wipe always
+echo "name=swap,type=swap,size=+1GiB" | sfdisk /dev/sda --append --quiet --wipe always
+echo "name=root,type=linux,size=+10GiB" | sfdisk /dev/sda --append --quiet --wipe always
+echo "name=home,type=linux" | sfdisk /dev/sda --append --quiet --wipe always
 sfdisk /dev/sda --list --verify
