122e7719688fa21f7ef1e02365935019d5bd7a8b
ugoat <ugo.schieli@gmail.com>
2023-10-09 21:26:16 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -49,7 +49,6 @@ mount --mkdir $ESP /mnt/boot
 mount --mkdir $HOME_PART /mnt/home
 swapon $SWAP_PART
 
-reflector --country fr,de --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 sed '/ParallelDownloads/s/^#//' -i /etc/pacman.conf
 pacstrap -K /mnt `grep -v '^#' ./pkglist.txt`
 
