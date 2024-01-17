4e29e9a8d6b2afe5ef6f2ff753e8c7a87adde7bd
Ugo <ugo.schieli@epitech.eu>
2024-01-17 20:13:49 +0100

    install script for gemini base server

--- src/chroot.sh
+++ src/chroot.sh
@@ -18,25 +18,34 @@ cat << EOF > /etc/hostname
 gemini
 EOF
 
-cp /archinstall/configs/00-keyboard.conf /etc/X11/xorg.conf.d/
-cp /archinstall/configs/30-touchpad.conf /etc/X11/xorg.conf.d/
+cat << EOF > /etc/systemd/network/25-wireless.network
+[Match]
+Name=wlp1s0
 
-systemctl enable NetworkManager.service
-systemctl enable bluetooth.service
+[Network]
+DHCP=yes
+MulticastDNS=yes
+EOF
+
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+systemctl enable iwd.service
 systemctl enable docker.service
 
 passwd
 
-useradd -m -G wheel,docker ugo
+useradd -m -G wheel,docker bite
 sed '/%wheel ALL=(ALL:ALL) ALL/s/^# //' -i /etc/sudoers
-passwd ugo
+passwd bite
 
 sed '/Color/s/^#//' -i /etc/pacman.conf
 sed '/ParallelDownloads/s/^#//' -i /etc/pacman.conf
 
 bootctl install
+systemctl enable systemd-boot-update.service
 cat << EOF > /boot/loader/loader.conf
 default arch.conf
+timeout 0
 EOF
 cat << EOF > /boot/loader/entries/arch.conf
 title     Arch Linux
@@ -45,4 +54,3 @@ initrd    /amd-ucode.img
 initrd    /initramfs-linux.img
 options   root=PARTLABEL=root rw quiet
 EOF
-systemctl enable systemd-boot-update.service
--- src/install.sh
+++ src/install.sh
@@ -2,65 +2,39 @@
 
 set -xeuo pipefail
 
-CONFIG=virtualbox
+DISK=/dev/nvme0n1;
+EFI_PART="${DISK}p1";
+SWAP_PART="${DISK}p2";
+ROOT_PART="${DISK}p3";
+EFI_SIZE=512;
+SWAP_SIZE=10;
+ROOT_SIZE=50;
 
-if [[ $CONFIG == "gemini" ]]; then
-    DISK=/dev/nvme0n1;
-    ESP="${DISK}p1";
-    SWAP_PART="${DISK}p2";
-    ROOT_PART="${DISK}p3";
-    HOME_PART="${DISK}p4";
-    ESP_SIZE=500;
-    SWAP_SIZE=10;
-    ROOT_SIZE=50;
-elif [[ $CONFIG == "qemu" ]]; then
-    DISK=/dev/vda;
-    ESP="${DISK}1";
-    SWAP_PART="${DISK}2";
-    ROOT_PART="${DISK}3";
-    HOME_PART="${DISK}4";
-    ESP_SIZE=500;
-    SWAP_SIZE=1;
-    ROOT_SIZE=10;
-elif [[ $CONFIG == "virtualbox" ]]; then
-    DISK=/dev/sda;
-    ESP="${DISK}1";
-    SWAP_PART="${DISK}2";
-    ROOT_PART="${DISK}3";
-    HOME_PART="${DISK}4";
-    ESP_SIZE=500;
-    SWAP_SIZE=1;
-    ROOT_SIZE=10;
-fi
-
-echo "name=esp,type=uefi,bootable,size=+${ESP_SIZE}MiB" | sfdisk /dev/sda --quiet --wipe always
-echo "name=swap,type=swap,size=+${SWAP_SIZE}GiB" | sfdisk /dev/sda --append --quiet --wipe always
-echo "name=root,type=linux,size=+${ROOT_SIZE}GiB" | sfdisk /dev/sda --append --quiet --wipe always
-echo "name=home,type=linux" | sfdisk /dev/sda --append --quiet --wipe always
+echo "name=esp,type=uefi,bootable,size=+${EFI_SIZE}MB" | sfdisk /dev/sda --quiet --wipe always
+echo "name=swap,type=swap,size=+${SWAP_SIZE}GB" | sfdisk /dev/sda --append --quiet --wipe always
+echo "name=root,type=linux,size=+${ROOT_SIZE}GB" | sfdisk /dev/sda --append --quiet --wipe always
 sfdisk /dev/sda --list --verify
 
-mkfs.fat -F 32 -n "efi" $ESP
+mkfs.fat -F 32 -n "efi" $EFI_PART
 mkswap -L "swap" $SWAP_PART
 mkfs.ext4 -L "root" $ROOT_PART
-mkfs.ext4 -L "home" $HOME_PART
 
 mount $ROOT_PART /mnt
-mount --mkdir $ESP /mnt/boot
-mount --mkdir $HOME_PART /mnt/home
+mount --mkdir $EFI_PART /mnt/boot
 swapon $SWAP_PART
 
 sed '/ParallelDownloads/s/^#//' -i /etc/pacman.conf
-pacstrap -K /mnt `grep -v '^#' ./pkglist.txt`
+pacstrap -K /mnt base linux linux-firmware amd-ucode iwd vim openssh docker
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-mkdir /mnt/archinstall
-cp chroot.sh /mnt/archinstall
-cp -r configs /mnt/archinstall
-arch-chroot /mnt /archinstall/chroot.sh
+ln -sf /run/systemd/resolve/stub-resolv.conf /mnt/etc/resolv.conf
 
-umount $ESP
-umount $HOME_PART
+mkdir /mnt/root/archinstall
+cp chroot.sh /mnt/root/archinstall
+arch-chroot /mnt /root/archinstall/chroot.sh
+
+umount $EFI_PART
 umount $ROOT_PART
 swapoff $SWAP_PART
 
