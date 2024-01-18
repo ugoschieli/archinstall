e0fe667dd44564fbe84680fbf5243db2cb48adc0
Ugo <ugo.schieli@epitech.eu>
2024-01-18 21:58:51 +0100

    forgot docker-compose package

--- src/install.sh
+++ src/install.sh
@@ -23,7 +23,7 @@ mount --mkdir $EFI_PART /mnt/boot
 swapon $SWAP_PART
 
 sed '/ParallelDownloads/s/^#//' -i /etc/pacman.conf
-pacstrap -K /mnt base linux linux-firmware amd-ucode iwd vim openssh docker sudo polkit htop bash-completion
+pacstrap -K /mnt base linux linux-firmware amd-ucode iwd vim openssh docker docker-compose sudo polkit htop bash-completion
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
